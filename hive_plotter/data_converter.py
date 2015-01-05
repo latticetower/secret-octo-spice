9b6c15ff314a3ef408ffde65a9071d728c294fee
Tatiana Malygina <merlettaia@gmail.com>
2015-01-06 02:16:30 +0300

    added modified loader

--- hive_plotter/data_converter.py
+++ hive_plotter/data_converter.py
@@ -7,50 +7,61 @@ class DataConverter(object):
   def __init__(self, target_file):
     self.source_files = []
     self.target_file = target_file
-  def add_source(self, source_file):
-      self.source_files.append(source_file)
+  def add_source(self, source_file, alias):
+      self.source_files.append([source_file, alias])
     #
   def convert_and_save(self):
     self.convert()
     self.save()
-  def get_dict(self, line_data):
+  def get_dict(self, source_file, alias, line_data):
       data = OrderedDict()
-      data["name"] = line_data[0]
+      data["name"] = "{0}.{1}.{2}.{3}".format(alias, line_data[0], line_data[1], line_data[-2])
       data["size"] = long(line_data[2]) - long(line_data[1])
-      data["strand"] = line_data[3]
-      data["commonName"] = line_data[4]
-      data["group"] = line_data[5]
+      #data["strand"] = line_data[3]
+      data["commonName"] = line_data[-2]
+      data["group"] = line_data[-1]
+      data["file"] = alias
+      data["source_file"] = source_file
+      data["type"] = alias
       return data
   def convert(self):
       self.data = OrderedDict()
-      for source_file in self.source_files:
+      for [source_file, alias] in self.source_files:
           with open(source_file) as f:
-              self.data[source_file] = []
+              if not(alias in self.data):
+                  self.data[alias] = []
               for line in f:
                   line_data = line.strip('\r\n').split()
                   if line_data[0] == "chr":
                       continue
-                  self.data[source_file].append(self.get_dict(line_data))
+                  self.data[alias].append(self.get_dict(source_file, alias, line_data))
   def save(self):
       output_file = open(self.target_file, 'w')
       output_file.write('[\n')
       # for each chromosome in list print sequence to output file
-      for file in self.data.keys():
-          output_file.write((", ").join([
-                self.data_to_string(file, record) for record in self.data[file]
-            ]))
+      output_file.write(
+        (", ").join(
+            [
+                self.data_to_string(file, record)
+                for file in self.data.keys()
+                for record in self.data[file]
+            ]
+            ))
       output_file.write(']\n')
       output_file.close()
   def data_to_string(self, source, record):
      return "{\n"+(
         ",\n".join(
-            [ "  \"{0}\": \"{1}\"".format(key, record[key]) for key in record.keys() ] + ["  \"{0}\": \"{1}\"".format("source", source),
-            "\"imports\": []"] 
-
+            [ "  \"{0}\": \"{1}\"".format(key, record[key]) for key in record.keys() ]
         ) )+"\n}"
 
 
 converter = DataConverter("species.json")
-converter.add_source("data/catGenesGroupB2_1corrected.bed")
-#converter.add_source("data/catGenesGroupB2_1corrected.bed")
+converter.add_source("data/catGenesGroupB2_1corrected.bed", "cat")
+converter.add_source("data/catGenesGroupB2_2corrected.bed", "cat")
+converter.add_source("data/cheetahGenesB2corrected.bed", "cheetah")
+converter.add_source("data/dogGenesGroup12corrected.bed", "dog")
+converter.add_source("data/dogGenesGroup35corrected.bed", "dog")
+converter.add_source("data/humanGnesGroup6corrected.bed", "human")
+
 converter.convert_and_save()

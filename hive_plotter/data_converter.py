9f9f483dcc38412e72b94ec5a60aafc9f3a18e75
Tatiana Malygina <merlettaia@gmail.com>
2015-01-08 17:48:20 +0300

    modified dataset, now links are shown only between orthologs with same commonName

--- hive_plotter/data_converter.py
+++ hive_plotter/data_converter.py
@@ -8,8 +8,9 @@ class DataConverter(object):
     self.source_files = []
     self.target_file = target_file
     self.groups = {}
-  def add_source(self, source_file, alias):
-      self.source_files.append([source_file, alias])
+    self.commonNames = {}
+  def add_source(self, source_file, alias, data_sort_flag = True):
+      self.source_files.append([source_file, alias, data_sort_flag])
     #
   def convert_and_save(self):
     self.convert()
@@ -22,6 +23,12 @@ class DataConverter(object):
       data["start"] = line_data[1]
       #data["strand"] = line_data[3]
       data["commonName"] = line_data[-2]
+      if not (data["commonName"] in self.commonNames):
+          self.commonNames[data["commonName"]] = OrderedDict()
+      if (not alias in self.commonNames[data["commonName"]]):
+          self.commonNames[data["commonName"]][alias] = [data["name"]]
+      else:
+          self.commonNames[data["commonName"]][alias].append(data["name"])
       data["group"] = line_data[-1]
       if not (data["group"] in self.groups):
           self.groups[data["group"]] = OrderedDict()
@@ -33,17 +40,20 @@ class DataConverter(object):
       return data
   def convert(self):
       self.data = OrderedDict()
-      for [source_file, alias] in self.source_files:
+      self.data_sort = {}
+      for [source_file, alias, data_sort_flag] in self.source_files:
           with open(source_file) as f:
               if not(alias in self.data):
                   self.data[alias] = []
+                  self.data_sort[alias] = data_sort_flag
               for line in f:
                   line_data = line.strip('\r\n').split()
                   if line_data[0] == "chr":
                       continue
                   self.data[alias].append(self.get_dict(source_file, alias, line_data))
       for k in self.data.keys():
-          self.data[k].sort( key=itemgetter('chr', 'start'))
+          if self.data_sort[k]:
+              self.data[k].sort(key = itemgetter('chr', 'start'))
   def save(self):
       output_file = open(self.target_file, 'w')
       output_file.write('[\n')
@@ -62,11 +72,11 @@ class DataConverter(object):
       return "{\n"+(
         ",\n".join(
             [ "  \"{0}\": \"{1}\"".format(key, record[key]) for key in record.keys() ] + [
-                "  \"imports\": [\n    {0}\n    ]".format(
+                "  \"gene_links\": [\n    {0}\n    ]".format(
                     ",\n    ".join([
                         "\"{0}\"".format(
                             value
-                            ) for value in self.groups[record["group"]].values()] + [
+                            ) for value in sum(self.commonNames[record["commonName"]].values(), [])] + [
                                 "\"{0}\"".format(record["name"])
                             ]
                         )
@@ -78,7 +88,7 @@ class DataConverter(object):
 converter = DataConverter("species.json")
 converter.add_source("data/catGenesGroupB2_1corrected.bed", "cat")
 converter.add_source("data/catGenesGroupB2_2corrected.bed", "cat")
-converter.add_source("data/cheetahGenesB2corrected.bed", "cheetah")
+converter.add_source("data/cheetah_mhc_corrected_map.txt", "cheetah", False)
 converter.add_source("data/dogGenesGroup12corrected.bed", "dog")
 converter.add_source("data/dogGenesGroup35corrected.bed", "dog")
 converter.add_source("data/humanGnesGroup6corrected.bed", "human")
